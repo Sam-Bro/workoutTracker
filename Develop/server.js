23368a2d5d25a30d3ddf683ca6d7023c91ff2033
OrbmasWin <sambrostudent@gmail.com>
2020-06-17 14:38:10 +0800

    created basic server

---
+++ Develop/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+
+let app = express();
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static("public"));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("app running on: " + PORT);
+})
